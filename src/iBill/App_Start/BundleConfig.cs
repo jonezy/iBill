09eb98acc2190f994059ed102f3cc0921cb234da
Chris Jones <chris@jonezy.org>
2013-09-19 06:14:13 -0400

    more basic setup!
    Signed-off-by: Chris Jones <chris@jonezy.org>

---
+++ src/iBill/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace iBill {
+  public class BundleConfig {
+    // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+    public static void RegisterBundles(BundleCollection bundles) {
+
+      bundles.Add(new ScriptBundle("~/bundles/base-js").Include(
+        "~/Public/js/vendor/jquery-1.9.2.min.js",
+        "~/Public/js/vendor/bootstrap.min.js"));
+
+      bundles.Add(new StyleBundle("~/Public/css/base-css").Include(
+        "~/Public/css/vendor/bootstrap.min.css",
+        "~/Public/css/iBill.css"));
+    }
+  }
+}
