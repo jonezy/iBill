c9c2602ef38bdc189a34219f896caa859c6c81cf
Chris Jones <chris@jonezy.org>
2013-09-23 20:26:41 -0400

    got the data layer working
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- src/iBill/Controllers/HomeController.cs
+++ src/iBill/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using iBill.Data;
 
 namespace iBill.Controllers {
   public class HomeController : Controller {
 
     public ActionResult Index() {
+
       return View();
     }
 
